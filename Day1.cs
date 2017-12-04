be21c9479455f019ca40d0a0e4cdb259262b93e7
Smurfa <pontus.franzen@tretton37.com>
2017-12-04 13:48:17 +0100

    Add day 1, task 1 solution.

---
+++ Day1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2017
+{
+    public class Day1
+    {
+        public int Task1(string value)
+        {
+            var numbers = value.ToCharArray().CharToIntegerEnumerable();
+            var sum = 0;
+
+            for (var i = 0; i < numbers.Count() - 1; i++)
+            {
+                if (numbers.ElementAt(i) == numbers.ElementAt(i + 1))
+                    sum += numbers.ElementAt(i);
+            }
+            if (numbers.ElementAt(0) == numbers.ElementAt(numbers.Count() - 1))
+                sum += numbers.ElementAt(0);
+
+            return sum;
+        }
+    }
+}
---
+++ EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2017
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<int> CharToIntegerEnumerable(this IEnumerable<char> input)
+        {
+            return input.Select(x => int.Parse(x.ToString()));
+        }
+    }
+}
