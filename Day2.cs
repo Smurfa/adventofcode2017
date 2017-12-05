a3374802e07e2b7c39d561f3b9cfa1fea5519a68
Smurfa <pontus.franzen@tretton37.com>
2017-12-05 10:06:49 +0100

    Solution Day2 Task1.

---
+++ Day2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2017
+{
+    public class Day2
+    {
+        private readonly IEnumerable<IEnumerable<int>> _data;
+
+        public Day2(string filepath)
+        {
+            _data = ReadInput(filepath);
+        }
+
+        public int Task1()
+        {
+            return _data.Sum(x => x.Max() - x.Min());
+        }
+
+        private IEnumerable<IEnumerable<int>> ReadInput(string filepath)
+        {
+            var lines = File.ReadAllText(filepath).Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            return lines.Select(line => line.Split('\t').Select(int.Parse));
+        }
+    }
+}
